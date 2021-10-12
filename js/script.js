25157283b7d1da814b8fbf0a9707783e015eb0c7
Daniyal-Shah <daniyalshah7862018@gmail.com>
2021-10-12 11:53:17 +0500

    jQuery Practice

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(function() {
+  // jQuery goes here...
+
+  // Uncomment this line to fade out the red box on page load
+  // $(".red-box").fadeOut(2000);
+  // $('.blue-box').show();
+  // $('.blue-box').toggle();
+
+
+  // $('.blue-box').slideUp(4000);
+  // $('.blue-box').slideDown(4000);
+
+  // $('p').hide();
+  // $('p').slideDown(2000);
+
+  // $('.blue-box').animate({
+  //   "margin-left":"+=200px"
+  // },3000,"linear");
+
+  // $('.blue-box').animate({
+  //   "margin-left":"+=200px",
+  //   "height":"50px",
+  //   "width": "50px",
+  //   "opacity":"0"
+  // },2000,"swing");
+
+  // $(".red-box").fadeTo(1000,0.2);
+  // $(".green-box").delay(1000).fadeTo(1000,0.45);
+  // $(".blue-box").delay(2000).fadeTo(1000,0.6).faceOut().delay(1000).fadeIn();    
+
+
+
+  // $(".red-box").fadeTo(1000,0.2, ()=>
+  // {
+  //   $(".green-box").fadeTo(1000,0.45,()=>{
+  //     $(".blue-box").fadeTo(1000,0.6);
+  //   })
+  // });
+
+
+  // $('p').css('background-color', 'yellow');
+  // $('p:last').css('background-color', 'green');
+  // $('li').css('background-color', 'blue');
+  // $('li:even').css('background-color', 'purple');
+  // $('input:password').css('background-color', 'brown');
+
+
+
+  
+});
